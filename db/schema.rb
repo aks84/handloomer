964f2d9f3c26df14bf89d7ca820451d74d1817be
Amulya Kumar Shahi <amulyaa_24@yahoo.in>
2020-12-30 16:18:11 +0530

    display 1st image on product index and removed image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_151608) do
+ActiveRecord::Schema.define(version: 2020_12_30_100801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2020_12_28_151608) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
     t.json "images"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
